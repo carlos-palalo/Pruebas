5c12e85b31e0d37275214012bf934aa1fe4da920
carlos-palalo <palaxcarlos@gmail.com>
2020-09-22 20:07:43 +0200

    cccc

---
+++ prueba.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Tipos de variables en PHP</title>
+</head>
+
+<body>
+    <?php
+    $a = 137;
+    $b = "9.99";
+    $c = "ejemplo variables PHP";
+    //impresion
+    echo $a . "<br>" . $b . "<br>" . $c;
+    ?>
+</body>
+
+</html>
